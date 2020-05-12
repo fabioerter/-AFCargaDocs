ad7b40a925e76d41a50cf01278f2d4a2b00500fe
AnaQuinteroT <anaq.88@live.com>
2020-05-12 14:15:46 -0500

    Header y footer front

--- AFCargaDocs/Models/Entidades/GlobalVariables.cs
+++ AFCargaDocs/Models/Entidades/GlobalVariables.cs
@@ -14,12 +14,14 @@ namespace AFCargaDocs.Models.Entidades
         private static string fndc;
         private static string aidy;
         private static string aidp;
+        private static string aplicacion;
 
 
         public static string Matricula { get => matricula; set => matricula = value; }
         public static string Fndc { get => fndc; set => fndc = value; }
         public static string Aidy { get => aidy; set => aidy = value; }
         public static string Aidp { get => aidp; set => aidp = value; }
+        public static string Aplicacion { get => aplicacion; set => aplicacion = value; }
         public static string getPdim( string matricula)
         {
             using (OracleConnection cnx = new OracleConnection(ConfigurationManager.ConnectionStrings["Banner"].ConnectionString))
